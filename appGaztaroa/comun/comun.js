3b2ee2bc6cc871042284bae505c9509b1c638891
naiarapla <naiaraplatero@gmail.com>
2021-06-07 23:20:29 +0200

    Firebase

--- appGaztaroa/comun/comun.js
+++ appGaztaroa/comun/comun.js
@@ -1,4 +1,5 @@
 //export const baseUrl = 'http://192.168.0.13:3001/';
-export const baseUrl = 'http://172.20.10.4:3001/';
+//export const baseUrl = 'http://172.20.10.4:3001/';
+export const baseUrl = 'https://dsm-react-native-default-rtdb.europe-west1.firebasedatabase.app/';
 export const colorGaztaroaOscuro = '#015afc';
 export const colorGaztaroaClaro = '#c2d3da';
--- appGaztaroa/redux/comentarios.js
+++ appGaztaroa/redux/comentarios.js
@@ -12,6 +12,17 @@ export const comentarios = (state = { errMess: null, comentarios:[]}, action) =>
 
     case ActionTypes.ADD_COMENTARIO:
      let id=state.comentarios.length;
+     fetch('https://dsm-react-native-default-rtdb.europe-west1.firebasedatabase.app/comentarios/' + id + '.json', {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      
+      body: JSON.stringify({
+        id: id, excursionId: action.excursionId, valoracion: action.valoracion, autor: action.autor, comentario: action.comentario,dia: action.dia
+      })
+    });
      return {...state,errMess: null, comentarios: 
       [...state.comentarios, 
         {id: id, excursionId: action.excursionId, valoracion: action.valoracion, autor: action.autor, comentario: action.comentario,dia: action.dia} 
